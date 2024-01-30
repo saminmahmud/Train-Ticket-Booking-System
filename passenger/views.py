f23d39615fdd274131592845ae209e2edf7050dc
saminmahmud <samin.mahmud18@gmail.com>
2024-01-30 11:24:04 +0600

    email fixed

--- passenger/views.py
+++ passenger/views.py
@@ -35,7 +35,7 @@ class UserRegistrationView(FormView):
         token = default_token_generator.make_token(user)
         uid = urlsafe_base64_encode(force_bytes(user.pk))
         # confirm_link = f"http://127.0.0.1:8000/account/active/{uid}/{token}"
-        confirm_link = f"https://bangladesh-railway-gpkx.onrender.com/account/active/{uid}/{token}"
+        confirm_link = f"https://bangladesh-railway-h8hw.onrender.com/account/active/{uid}/{token}"
         email_subject = "Confirm Your Email"
         email_body = render_to_string('confirm_email.html', {'link': confirm_link})
         email = EmailMultiAlternatives(email_subject, '', to=[user.email])
